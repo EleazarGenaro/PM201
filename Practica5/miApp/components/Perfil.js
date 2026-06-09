d9644d5c290bc351743afea8a9505367278b5126
EleazarGenaro <122043775@upq.edu.mx>
2026-06-08 22:31:41 -0600

    Practica 6 Terminada

---
+++ Practica5/miApp/components/Perfil.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+export const Perfil= () => {
+    return(
+        <View>  
+            <Text>Nombre: Eleazar Genaro Vera Piña</Text>
+            <Text>Carrera: Ingeniería en Sistemas Computacionales</Text>
+            <Text>Materia: Programación Móvil</Text>
+            <Text>Cuatrimestre: Noveno</Text>
+        </View>
+        
+    );
+}
